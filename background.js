64aced0a6d8e2a5c9813a64edac35d388b0e9347
Manas Rawat <manasrawat.cse@gmail.com>
2022-07-06 00:56:46 +0530

    basic functionality completed

---
+++ background.js
@@ -0,0 +1,85 @@
+const url = window.location.href;
+
+var id;
+var index;
+
+if(url[23] == 'c'){
+    id = url.slice(31, 35);
+    index = url.slice(44, 45);
+}
+else{
+    id = url.slice(42, 46);
+    index = url.slice(47, 48);
+}
+
+const requestURL = `https://codeforces.com/api/contest.standings?contestId=${id}&from=1&count=1`;
+
+fetchData();
+
+async function fetchData(){
+    var rating = null;
+
+    try{
+        const response = await fetch(requestURL);
+        const data = await response.json();
+        const problemsList = data.result.problems;
+
+        if(data.status == "OK"){
+            for(var i = 0; i < problemsList.length; i++){
+                if(problemsList[i].index == index){
+                    rating = problemsList[i].rating;
+                    break;
+                }
+            }
+        }
+        else{
+            rating = -1;
+        }
+    }
+    catch(e){
+        rating = -1;
+    }
+
+    var toInsert;
+    if(rating != null && rating > -1){
+        toInsert = `
+                            <div class="roundbox sidebox" style="">
+                                <div class="roundbox-lt">&nbsp;</div>
+                                <div class="roundbox-rt">&nbsp;</div>
+                                <div class="caption titled">→ CF GetRating
+                                    <div class="top-links"></div>
+                                </div>
+                                <div style="padding: 0.5em; height: 27px">
+                                    <div class="roundbox " style="margin:2px; padding:0 3px 2px 3px; background-color:#f0f0f0;float:left;">
+                                        <div class="roundbox-lt">&nbsp;</div>
+                                        <div class="roundbox-rt">&nbsp;</div>
+                                        <div class="roundbox-lb">&nbsp;</div>
+                                        <div class="roundbox-rb">&nbsp;</div>
+                                        <span class="tag-box" style="font-size:1.2rem;" title="Difficulty">
+                                            *${rating}
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+                        `
+    }
+    else if(rating == -1){
+        toInsert = `
+                        <div class="roundbox sidebox" style="">
+                            <div class="roundbox-lt">&nbsp;</div>
+                            <div class="roundbox-rt">&nbsp;</div>
+                            <div class="caption titled">→ CF GetRating
+                                <div class="top-links"></div>
+                            </div>
+
+                            <div>
+                                <div style="margin:1em;font-size:0.8em;color: red;">
+                                    Codeforces API Error
+                                </div>
+                             </div>
+                        </div>
+                    `
+    }
+
+    document.querySelector("#sidebar").innerHTML += toInsert;
+}
