13d0103f92eca242ed79ed6de0867f96ce4d0dbb
Manas Rawat <manasrawat.cse@gmail.com>
2022-07-22 23:31:36 +0530

    show all tags option added

--- background.js
+++ background.js
@@ -22,6 +22,7 @@ fetchData();
 
 async function fetchData(){
     var rating = null;
+    var tags = null;
 
     try{
         const response = await fetch(requestURL);
@@ -32,6 +33,7 @@ async function fetchData(){
             for(var i = 0; i < problemsList.length; i++){
                 if(problemsList[i].index == index){
                     rating = problemsList[i].rating;
+                    tags = problemsList[i].tags;
                     break;
                 }
             }
@@ -91,8 +93,8 @@ async function fetchData(){
                             <div class="caption titled">→ CF GetRating
                                 <div class="top-links"></div>
                             </div>
-                            <div style="padding: 0.5em;margin-bottom: 25px;">
-                                <div class="roundbox " style="margin:2px; padding:0 3px 2px 3px; background-color:#f0f0f0;float:left;">
+                            <div style="padding: 0.5em;">
+                                <div class="roundbox" style="margin:2px; padding:0 3px 2px 3px; background-color:#f0f0f0;float:left;">
                                     <div class="roundbox-lt">&nbsp;</div>
                                     <div class="roundbox-rt">&nbsp;</div>
                                     <div class="roundbox-lb">&nbsp;</div>
@@ -101,13 +103,55 @@ async function fetchData(){
                                         *${rating}
                                     </span>
                                 </div>
+
+                    `
+
+        tags.forEach((tag) => {
+            toInsert += `
+                            <div class="roundbox hidden" style="margin:2px; padding:0 3px 2px 3px; background-color:#f0f0f0;float:left;">
+                                <div class="roundbox-lt">&nbsp;</div>
+                                <div class="roundbox-rt">&nbsp;</div>
+                                <div class="roundbox-lb">&nbsp;</div>
+                                <div class="roundbox-rb">&nbsp;</div>
+                                <span class="tag-box" style="font-size:1.2rem;" title="Difficulty">
+                                    ${tag}
+                                </span>
                             </div>
-                            <div style="text-align:center;margin-bottom:15px">
-                                <a href="https://codeforces.com/contest/${id}/standings" target="_blank"><button>Contest Standings</button></a>
+                        `
+        });
+
+        toInsert += `
+                                <div style="clear:both;text-align:right;font-size:1.1rem;">
+                                </div>
                             </div>
+
+                            <div style="text-align:center;">
+                                <button onclick='
+                                                    document.querySelectorAll(".hidden").forEach((item) => {
+                                                        item.classList.remove("hidden");
+                                                    });
+                                                ' 
+                                         style="margin-bottom:3px; width: 50%;">Show All Tags</button>
+                            </div>
+                            <div style="text-align:center;">
+                                <a href="https://codeforces.com/contest/${id}/standings" target="_blank"><button style="margin-bottom:15px; margin-top: 3px; width: 50%;">Contest Standings</button></a>
+                            </div>
+
                         </div>
                     `
     }
 
-    document.querySelector("#sidebar").innerHTML += toInsert;
+    const getRatingBox = document.createElement("div");
+    getRatingBox.innerHTML = toInsert;
+    document.querySelector("#sidebar").appendChild(getRatingBox);
 }
+
+{/* <div class="roundbox tagBoxGetRating" style="margin:2px; padding:0 3px 2px 3px; background-color:#f0f0f0;float:left; display: none;">
+<div class="roundbox-lt">&nbsp;</div>
+<div class="roundbox-rt">&nbsp;</div>
+<div class="roundbox-lb">&nbsp;</div>
+<div class="roundbox-rb">&nbsp;</div>
+<span class="tag-box" style="font-size:1.2rem;" title="Difficulty">
+    ${tag}
+</span>
+</div> */}
