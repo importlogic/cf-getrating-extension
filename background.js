6d9258e779ad958b1b2c6f1a74be4ad391689975
Manas Rawat <manasrawat.cse@gmail.com>
2022-07-22 23:37:24 +0530

    show other tags even when rating is not available

--- background.js
+++ background.js
@@ -49,22 +49,52 @@ async function fetchData(){
     var toInsert;
     if(rating == null){
         toInsert = `
-                        <div class="roundbox sidebox" style="">
+                    <div class="roundbox sidebox" style="">
+                        <div class="roundbox-lt">&nbsp;</div>
+                        <div class="roundbox-rt">&nbsp;</div>
+                        <div class="caption titled">→ CF GetRating
+                            <div class="top-links"></div>
+                        </div>
+                        <div style="padding: 0.5em;">
+                            <div style="margin-bottom: 5px; font-size:0.8em;color: red;">
+                                Rating not available for this question.
+                            </div>
+
+                    `
+
+    tags.forEach((tag) => {
+        toInsert += `
+                        <div class="roundbox hidden" style="margin:2px; padding:0 3px 2px 3px; background-color:#f0f0f0;float:left;">
                             <div class="roundbox-lt">&nbsp;</div>
                             <div class="roundbox-rt">&nbsp;</div>
-                            <div class="caption titled">→ CF GetRating
-                                <div class="top-links"></div>
-                            </div>
-                            <div>
-                                <div style="margin:1em;font-size:0.8em;color: red;">
-                                    Rating not available for this question.
-                                </div>
-                            </div>
-                            <div style="text-align:center;margin-bottom:15px">
-                                <a href="https://codeforces.com/contest/${id}/standings" target="_blank"><button>Contest Standings</button></a>
-                            </div>
+                            <div class="roundbox-lb">&nbsp;</div>
+                            <div class="roundbox-rb">&nbsp;</div>
+                            <span class="tag-box" style="font-size:1.2rem;" title="Difficulty">
+                                ${tag}
+                            </span>
                         </div>
                     `
+    });
+
+    toInsert += `
+                            <div style="clear:both;text-align:right;font-size:1.1rem;">
+                            </div>
+                        </div>
+
+                        <div style="text-align:center;">
+                            <button onclick='
+                                                document.querySelectorAll(".hidden").forEach((item) => {
+                                                    item.classList.remove("hidden");
+                                                });
+                                            ' 
+                                     style="margin-bottom:3px; width: 50%;">Show All Tags</button>
+                        </div>
+                        <div style="text-align:center;">
+                            <a href="https://codeforces.com/contest/${id}/standings" target="_blank"><button style="margin-bottom:15px; margin-top: 3px; width: 50%;">Contest Standings</button></a>
+                        </div>
+
+                    </div>
+                `
     }
     else if(rating == -1){
         toInsert = `
